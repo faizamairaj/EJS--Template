3a462e6752b9d4bd2d6a77d1a0cd7ceda5c7ca5d
faizamairaj <faizamairaj0@gmail.com>
2025-07-02 11:15:44 +0500

    remove server less http

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     res.render('table', { num: number });
 });
 
-app.get('/table', (req, res) => {
+app.get('/', (req, res) => {
     // const number = parseInt(req.query.num);
     res.render('table');
 });
